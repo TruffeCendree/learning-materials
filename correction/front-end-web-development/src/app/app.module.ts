9f6645b9cf8ca31cdd4996e439ab68d27ef87074
Thomas VEILLARD <thomaveillard@live.fr>
2021-02-09 11:11:01 +0100

    Submit login over HTTP and handle success + error

--- correction/front-end-web-development/src/app/app.module.ts
+++ correction/front-end-web-development/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -32,7 +33,8 @@ import { ProfileComponent } from './profile/profile.component';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
