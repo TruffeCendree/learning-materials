9d5075597a424d82a3e81363bab1445243369adb
Thomas VEILLARD <thomaveillard@live.fr>
2021-02-11 09:06:53 +0100

    Show or hide 'Login'/'Profile' links depending existance of session

--- correction/front-end-web-development/src/app/navigation/navigation.component.ts
+++ correction/front-end-web-development/src/app/navigation/navigation.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import User from '@backend/models/user';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  // once initialized, you can use it to dynamically adapt the navigation template.
+  me: User;
+
+  constructor (activatedRoute: ActivatedRoute) {
+    // When the NavigationComponent is mounted, the MeService has already been resolved.
+    this.me = activatedRoute.snapshot.data.me;
+  }
+}
