bf79236edb6480168294c9fedc9816d08fc810e8
Thomas VEILLARD <thomaveillard@live.fr>
2021-02-12 19:06:00 +0100

    Add AuthenticatedGuard to /profile

---
+++ correction/front-end-web-development/src/app/authenticated.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { MeService } from './me.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticatedGuard implements CanActivate {
+  constructor (private meService: MeService, private router: Router) {}
+  
+  async canActivate() {
+    if (await this.meService.resolve()) return true;
+    return this.router.parseUrl('/login');
+  }
+}
